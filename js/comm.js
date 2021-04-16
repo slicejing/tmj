9abb645ecf180ac0af56af38d0d9025d05f4da3d
slice_jing <jingjing2lele@qq.com>
2021-04-16 17:17:06 +0800

    Add files via upload

---
+++ js/comm.js
@@ -0,0 +1,21 @@
+$(".van-tab").on("click", function() {
+	$(this).addClass("van-tab--active");
+	$(this).siblings(".van-tab").removeClass("van-tab--active");
+	console.log($(this).index())
+
+
+	$(".van-tabs__track").children(".van-tab__pane-wrapper").addClass("van-tab__pane-wrapper--inactive")
+		.hide();
+
+	$(".van-tabs__track").children(".van-tab__pane-wrapper").eq($(this).index())
+		.removeClass("van-tab__pane-wrapper--inactive")
+		.show();
+
+})
+$(".js-link").on("click", function() {
+	window.location.href = "login.html"
+})
+$("#homelink").on("click", function(e) {
+	e.stopPropagation();
+	window.location.href = "index.html"
+})
